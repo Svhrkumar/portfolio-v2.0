8af29fa08c9a8a5e61151e780d577ebe26836e48
svhrkumar544 <52437024+svhrkumar544@users.noreply.github.com>
2023-07-21 18:14:33 +0530

    initial commit with nav and hero component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './Pages/Home';
+import Header from './Components/Header';
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<Home />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,7 @@
+import Hero from '../Components/Hero';
+
+const Home = () => {
+	return <Hero />;
+};
+
+export default Home;
